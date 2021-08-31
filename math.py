936b550ade04dd284eee8338238481c8d835b328
vishwamtiwari <65553748+vishwamtiwari@users.noreply.github.com>
2021-08-31 18:54:35 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,22 @@
+# Add implementation
+def add(x,y):
+    return x+y
+
+# Subtract implementation
+def subtract(x,y):
+    return x-y          # on master branch
+
+# Multiply implementation
+def multiply(x,y):
+    return x*y          #on Bug456
+
+# Divide implementation      #on master
+def divide(x,y):
+    if y == 0:
+	return DIVIDE_BY_0_ERROR
+    else:
+	return x/y
+
+#square implementation
+def square(x):
+    return x*x
